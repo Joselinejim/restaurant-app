8200b8404c631f14a8b1decf558be2cd67ea6510
unknown <jimenezjoseline310@gmail.com>
2025-11-16 08:54:34 -0600

    Subiendo mis cambios

---
+++ database/migrations/2025_11_15_171509_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('table')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total',10,2)->default(0);
+            $table->enum('status', ['pendiente','recibido','en_preparacion','completado','cancelado'])->default('pendiente');
+            $table->timestamps();
+        });
+        Schema::create('order_itmes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->interger('quantity');
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
